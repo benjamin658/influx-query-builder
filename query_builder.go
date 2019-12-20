55c850b0dc8d92f3cc2f4a72a5f3e21bd3ed55ba
Ken Polizzi <ken.polizzi@jaguar-network.com>
2019-12-20 23:36:39 +0000

    fix support of unquoted value on time criteria

--- query_builder.go
+++ query_builder.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var validUnquotedTimeRegexp = regexp.MustCompile(`^\d+(ns|u|ms|s|m|h|d|w)?$`)
+
 // Duration Duration interface
 type Duration interface {
 	Nanoseconds(uint) Duration
@@ -511,6 +513,10 @@ func getCriteriaTemplate(tag Tag) string {
 	case bool:
 		return fmt.Sprintf(`"%s" %s %t`, tag.key, tag.op, tag.value)
 	default:
+		if tag.key == "time" && validUnquotedTimeRegexp.MatchString(tag.value.(string)) {
+			// 'time' key accepts non-quoted string value (eg: 1535313431000ns)
+			return fmt.Sprintf(`%s %s %s`, tag.key, tag.op, tag.value)
+		}
 		return fmt.Sprintf(`"%s" %s '%s'`, tag.key, tag.op, tag.value)
 	}
 }
